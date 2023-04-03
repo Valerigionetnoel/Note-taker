8958e6e43d0e17087211bb07aa4e1b3d3f40b299
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-02 21:22:03 -0300

    I try, now I need help

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+
+const dataBase = require('./db/db.json');
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('api/notes', (req, res) => {
+    console.info(`GET /api/notes`);
+    res.status(200).json(dataBase)
+});
+
+
+app.post('api/notes', (req, res) => {
+    console.info(`${req.methode} request received to add a note`);
+    const {title, text} = req.body
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+        console.log(response)
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('error in posting note')
+    }
+});
+
+// catch route send them the index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
