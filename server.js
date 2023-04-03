b1bb3fc1665c9da618d32c7132de3c953a4f93d9
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-03 16:31:34 -0300

    try to debug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const uid = require('uniqid')
 
 const dataBase = require('./db/db.json');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
